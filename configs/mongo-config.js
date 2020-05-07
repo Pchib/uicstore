1e206efd71d76170292baa838f3e03c3e9bc5655
chiboy <ugwuisaaciu@gmail.com>
2020-05-07 08:38:46 -0700

    change server gitignore file

---
+++ configs/mongo-config.js
@@ -0,0 +1,3 @@
+//replace <username> and <password> with your mongoDB atlas accounts info
+module.exports = "mongodb+srv://Uicstore:USxvuxFSxTXA1DGG@cluster0-kqhv5.mongodb.net/test?retryWrites=true&w=majority"
+
---
+++ configs/paypal-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'ATCngBYqQ04YvKlIT_LfBW-90VYLyUYAfggeQxKkRKLrPmI9MEZUJb5CLI3AgrhGyQn2RHclNJtPrcie',
+  'client_secret': 'EBcCBVjFY2s8m_qvn1cPAdK6UIewUH20H2kKFCWSMLdBfvHVcczGZy3PHK8RsGXRHPst1DxPrjSJvmiy'
+} 
